a045651d9578c6161fc959235ef357a5395b1348
GavinDmello <dmellogavin5000@gmail.com>
2015-08-20 01:08:39 +0530

    message was sent successfully bitch

--- try.php
+++ try.php
@@ -4,25 +4,27 @@ class Etzel {
 
 
 function isleep($qname) {
-	$x = new stdClass();
-	$x->qname=$qname;
-	$x->cmd = "ISLP";
+    // $x = new stdClass();
+    // $x->qname=$qname;
+    // $x->cmd = "ISLP";
     //$data = json_decode($x);
-    $tempData = html_entity_decode($x);
-     $cleanData = json_decode($tempData);
+    $query = array('qname' =>$qname ,'cmd'=>'ISLP');
+     $szStr = serialize($query);
+    
    // console.log(Obj.qname + Obj.cmd);
     $sock = socket_create(AF_INET, SOCK_STREAM, 0);
     socket_connect($sock , '127.0.0.1' , 80);
-   if( ! socket_send ( $sock , $data , strlen($cleanData) , 0))  {
+   if( ! socket_send ( $sock , $szStr , strlen($szStr) , 0))  {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
      
     die("Could not send data: [$errorcode] $errormsg \n");
-		}
-	}
+        }
+         echo "message was sent successfully bitch";
+    }
 
 
- 
+
 
 
 }
